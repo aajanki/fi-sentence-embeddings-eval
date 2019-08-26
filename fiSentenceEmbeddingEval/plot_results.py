8eb451f6db888af6c48e931109d6d2ee0cd56ea0
Antti Ajanki <antti.ajanki@iki.fi>
2019-08-26 18:33:14 +0300

    Plot only the macro F1 scores

--- fiSentenceEmbeddingEval/plot_results.py
+++ fiSentenceEmbeddingEval/plot_results.py
@@ -2,12 +2,14 @@ import argparse
 import math
 import os.path
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 
 def main():
     args = parse_args()
     df = load_results(args.resultdir)
+    df = select_scores_to_plot(df)
     g = draw_plots(df, 'score_mean')
     save_plot(g, os.path.join(args.resultdir, 'scores.svg'))
     save_plot(g, os.path.join(args.resultdir, 'scores.png'))
@@ -24,30 +26,58 @@ def load_results(resultdir):
     return pd.read_csv(filename)
 
 
+def select_scores_to_plot(df):
+    main_score_labels = [
+        ('Eduskunta-VKK', 'F1 score'),
+        ('Opusparcus', "Pearson's coefficient"),
+        ('TDT categories', 'F1 score'),
+        ('Ylilauta', 'Accuracy'),
+    ]
+
+    task_selectors = [
+        (df['task'] == x[0]) & (df['score_label'] == x[1])
+        for x in main_score_labels
+    ]
+
+    selected = df[pd.concat(task_selectors, axis=1).any(axis=1)]
+
+    for task, score_label in main_score_labels:
+        task_rows = selected[(selected['task'] == task) &
+                             (selected['score_label'] == score_label)]
+        if task_rows.empty:
+            print(f'Warning: No score {score_label} found for {task}!')
+
+    return selected
+
+
 def draw_plots(df, value_column, ylabel=None):
     models = df.model.unique()
     model_colors = ['C' + str(i + 1) for i in range(len(models))]
-    task_score_labels = {
-        x['task']: x['score_label']
-        for x in df[['task', 'score_label']]
-            .drop_duplicates()
-            .to_dict('records')
-    }
+    task_score_labels = (
+        df[['task', 'score_label']]
+        .drop_duplicates()
+        .to_dict('records'))
+    task_score_labels = sorted(task_score_labels,
+                               key=lambda x: (x['task'], x['score_label']))
 
     nrows = 2
     ncols = math.ceil(len(task_score_labels)/nrows)
     fig, ax = plt.subplots(nrows, ncols)
+    if ax.ndim < 2:
+        ax = ax[:, np.newaxis]
+
     fig.set_size_inches(8, 8)
-    for i, (t, task_ylabel) in enumerate(task_score_labels.items()):
-        tdata = df[df['task'] == t]
+    for i, labels in enumerate(task_score_labels):
+        tdata = df[(df['task'] == labels['task']) &
+                   (df['score_label'] == labels['score_label'])]
         y = [tdata[tdata['model'] == m][value_column].values[0]
              for m in models]
 
         a = ax[math.floor(i/ncols), i % ncols]
         a.bar(range(len(models)), y, tick_label=models, color=model_colors)
-        a.set_title(t)
+        a.set_title(labels['task'])
         a.set_xlabel('')
-        a.set_ylabel(ylabel or task_ylabel)
+        a.set_ylabel(ylabel or labels['score_label'])
         a.set_xticklabels(a.get_xticklabels(), rotation=90)
         a.spines['top'].set_visible(False)
         a.spines['right'].set_visible(False)
